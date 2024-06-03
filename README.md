4765faf128d48e1be13706269584b9171d3fbbf2
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-06-03 18:52:42 +0545

    desktop navbar

---
+++ README.md
@@ -0,0 +1,2 @@
+# igstoryviewer
+It is an anonymous Instagram story viewer application which let you view story of a person by pugging their username.
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const port = process.env.PORT || 3000;
+const path = require("path");
+const favicon = require("favicons")
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    const username = req.query.username;
+    res.render("index", {
+        name : username,
+    })
+})
+
+app.post("/", (req, res) => {
+    const username = req.body.username;
+    res.redirect(`/?username=${username}`);
+})
+
+app.listen(port, (req, res) => {
+    console.log(`server is running on port ${port}`);
+})
